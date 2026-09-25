e5f3a1c49d41b6594cc3c9e7006c127dcd630b7a
i-i-ijul <haqqikamvret@gmail.com>
2026-09-25 15:27:51 +0700

    mengubah iphone jadi android lagi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Iphone",
+                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
